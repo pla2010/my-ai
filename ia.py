7647fa4f9c71e44249b18346b6490618136b43b1
pla <100836804+pla2010@users.noreply.github.com>
2024-10-27 09:02:01 +0100

    Update ia.py

--- ia.py
+++ ia.py
@@ -79,7 +79,7 @@ def admin_dashboard():
         return redirect(url_for('admin'))
 
     data = load_questions()
-    return render_template('admin_dashboard.html', questions=data)
+     return jsonify(data)  # Renvoyer en JSON pour la requête GET
 
 # Route pour supprimer une question-réponse depuis le tableau de bord d'administration
 @app.route('/delete_question', methods=['POST'])
